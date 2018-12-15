d141515867f78fbceecec5ef697794fc4914f5ae
sunfen <sunfen@git>
2018-12-15 12:47:13 +0800

    deploy

--- src/main/java/cn/sf/sculpture/SculptureApplication.java
+++ src/main/java/cn/sf/sculpture/SculptureApplication.java
@@ -2,11 +2,14 @@ package cn.sf.sculpture;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages="cn.sf.sculpture.*")
+@EnableScheduling
 public class SculptureApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SculptureApplication.class, args);
 	}
 }
+
--- src/main/java/cn/sf/sculpture/common/SendSmsUtil.java
+++ src/main/java/cn/sf/sculpture/common/SendSmsUtil.java
@@ -1,20 +1,15 @@
 package cn.sf.sculpture.common;
 
-import java.net.URI;
-import java.nio.charset.Charset;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.io.IOException;
 
-import javax.annotation.PostConstruct;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+
+import com.github.qcloudsms.SmsSingleSender;
+import com.github.qcloudsms.SmsSingleSenderResult;
+import com.github.qcloudsms.httpclient.HTTPException;
 
 /**
  * @author sq mail:feifanhyx@foxmail.com
@@ -23,68 +18,39 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class SendSmsUtil {
 
-    @Autowired
-    private RestTemplate restTemplate;
+    @Value("${sculpture.sms.appid}")
+    private int appid;
+    
+    @Value("${sculpture.sms.appkey}")
+    private String appkey;
+    
+    @Value("${sculpture.sms.templateId}")
+    private int templateId;
 
-    @Value("${emms.sms.url}")
-    private String smsUrl;
-    
-    @Value("${emms.sms.uId}")
-    private String smsUid;
-    
-    @Value("${emms.sms.key}")
-    private String smsKey;
-    
-    private final StringBuffer path = new StringBuffer();
 
-    @PostConstruct
-    private void init() {
-        this.restTemplate.getMessageConverters().add(0, 
-            new StringHttpMessageConverter(Charset.forName("UTF-8")));
-    }
-    
-    /**
-     * 发送短信
-     * @param phones 手机号码
-     * @param msg 短信内容
-     * @return 发送是否成功，key：手机号，val：发送状态(ok：成功，其他都是失败)
-     */
-    public Map<String, HttpStatus> sendSMS(final Set<String> phones,final String msg) {
-        if(phones == null || phones.isEmpty() || msg == null || msg.isEmpty()) {
-            return null;
-        }
-        final Map<String, HttpStatus> result = new HashMap<>(phones.size());
+    // 需要发送短信的手机号码
+    String[] phoneNumbers = {"17772607725", "17731965725", "18211039521"};
+
+    public void sendMsg(){
         try {
-            final URI uri = new URI(this.getSendSMSURL(phones, msg));
-            final ResponseEntity<String> responseEntity = this.restTemplate.getForEntity(uri, String.class);
-            for (final String phone : phones) {
-                result.put(phone, responseEntity.getStatusCode());
-            }
-        } catch (Exception e) {
-            for (final String phone : phones) {
-                result.put(phone, HttpStatus.CONFLICT);
-            }
+            String[] params = {"5678", "10"};
+            
+            SmsSingleSender ssender = new SmsSingleSender(appid, appkey);
+         
+            SmsSingleSenderResult result =
+                ssender.sendWithParam("86", phoneNumbers[0], templateId, params, null, "", "");  
+        
+            System.out.println(result);
+        } catch (HTTPException e) {
+            // HTTP响应码错误
+            e.printStackTrace();
+        } catch (JSONException e) {
+            // json解析错误
+            e.printStackTrace();
+        } catch (IOException e) {
+            // 网络IO错误
+            e.printStackTrace();
         }
-        return result;
-    }
-    
-    private final String getSendSMSURL(final Set<String> phones, final String msg) {
-        if(this.path.length() == 0) {
-            this.path.append(this.smsUrl);
-            this.path.append("?userid=");
-            this.path.append(this.smsUid);
-            this.path.append("&password=");
-            this.path.append(this.smsKey);
-            this.path.append("&pszMobis=");
-        }
-        StringBuffer path = new StringBuffer(this.path);
-        phones.forEach(phone -> path.append(phone + ","));
-        path.deleteCharAt(path.length() - 1);
-        path.append("&pszMsg=");
-        path.append(msg);
-        path.append("&iMobiCount=" + phones.size());
-        path.append("&pszSubPort=*");
-        return path.toString();
     }
 
 }
