faec418c6c92ca1e7f6dd29f522973aca05e8802
sunfen <sunfen@git>
2019-02-12 17:02:42 +0800

    update

--- src/test/java/cn/sf/sculpture/SculptureApplicationTests2.java
+++ src/test/java/cn/sf/sculpture/SculptureApplicationTests2.java
@@ -3,10 +3,14 @@ package cn.sf.sculpture;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,57 +22,58 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SculptureApplicationTests {
+public class SculptureApplicationTests2 {
 
 
+    
     public static void main(String[] args) {  
         try {  
             //读取文件 转换成map‪
-            BufferedReader readerMap = 
-            new BufferedReader(new FileReader("C:/Users/DELL/Desktop/jobcontent-personnels.csv"));//换成你的文件名 
-            String lineMap = null;  
-            Map<String, String> map = new HashMap<>();
-            while((lineMap = readerMap.readLine()) != null){  
-                lineMap = lineMap.replace("(", "");
-                lineMap = lineMap.replace(")", "");
-                lineMap = lineMap.replace(";", "");
+           String path = "C:/Users/DELL/Desktop/jobtickets .csv";
+           InputStreamReader isr = new InputStreamReader(new FileInputStream(path), "UTF-8");
+           
+           BufferedReader readerMap = new BufferedReader(isr);
+           
+           String lineMap = null;  
+            
+           Map<String, String> map = new HashMap<>();
+           
+            while((lineMap = readerMap.readLine()) != null){
                 String item[] = lineMap.split(",");//CSV格式文件为逗号分隔符文件，这里根据逗号切分 
                 map.put(item[0], item[1]);
             }  
             
             List<String> writer = new ArrayList<>();
             
+            String path2 = "C:/Users/DELL/Desktop/jobticket-contents.csv";
+            InputStreamReader isr2 = new InputStreamReader(new FileInputStream(path2), "gbk");
             
+            BufferedReader reader = new BufferedReader(isr2);
             
-            BufferedReader reader = new BufferedReader(new FileReader("C:/Users/DELL/Desktop/jobcontentoperators.csv"));//换成你的文件名 
             String line = null;  
             while((line=reader.readLine())!=null){ 
-                line = line.replace("(", "");
-                line = line.replace(")", "");
-                line = line.replace(";", "");
                 String item[] = line.split(",");
                 if(map.keySet().contains(item[0])) {
-                    item[0] = map.get(item[0]);
+                    writer.add(line + "," + map.get(item[0]) + "\n");
                 }else {
+                    writer.add(line + "\n");
                    System.out.println(item[0]);
                 }
-                writer.add(item[0] + ","+ item[1]+ "\n");
             }
             
             
             
             try {  
-                File csv = new File("C:/Users/DELL/Desktop/writers.csv"); // CSV数据文件 
-           
-                BufferedWriter bw = new BufferedWriter(new FileWriter(csv, true)); // 附加 
+                String path1 = "C:/Users/DELL/Desktop/writers_content.csv"; // CSV数据文件 
+                OutputStreamWriter writerStream = new OutputStreamWriter(new FileOutputStream(path1),"UTF-8");
+                BufferedWriter bw = new BufferedWriter(writerStream);
                 for(String key : writer ) {
                     
                     // 添加新的数据行 
-                    bw.write( key);  
+                    bw.write(key);  
                 }
                 bw.newLine();  
                 bw.close();  
-           
               } catch (FileNotFoundException e) {  
                 // File对象的创建过程中的异常捕获 
                 e.printStackTrace();  
@@ -80,6 +85,5 @@ public class SculptureApplicationTests {
             e.printStackTrace();  
         }  
     } 
-    
 
 }
