f9b37e86526fca76efeb739a455739935853021a
sunfen <sunfen@git>
2019-02-22 18:19:30 +0800

    update

--- src/test/java/cn/sf/sculpture/SculptureApplicationTests.java
+++ src/test/java/cn/sf/sculpture/SculptureApplicationTests.java
@@ -3,14 +3,18 @@ package cn.sf.sculpture;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,10 +27,13 @@ public class SculptureApplicationTests {
 
     public static void main(String[] args) {  
         try {  
+            FileInputStream stream = new FileInputStream("C:\\Users\\DELL\\Desktop\\重庆数据20190221/specification.csv");
+            
             //读取文件 转换成map‪
-            BufferedReader readerMap = 
-            new BufferedReader(new FileReader("C:/Users/DELL/Desktop/jobcontent-personnels.csv"));//换成你的文件名 
+            BufferedReader readerMap = new BufferedReader(new InputStreamReader(stream,"gbk"));//换成你的文件名 
+          
             String lineMap = null;  
+            //KEY:speId  VALUE : speName
             Map<String, String> map = new HashMap<>();
             while((lineMap = readerMap.readLine()) != null){  
                 lineMap = lineMap.replace("(", "");
@@ -36,31 +43,79 @@ public class SculptureApplicationTests {
                 map.put(item[0], item[1]);
             }  
             
-            List<String> writer = new ArrayList<>();
+            Map<String, List<String>> results = new HashMap<>();
+          
             
             
-            
-            BufferedReader reader = new BufferedReader(new FileReader("C:/Users/DELL/Desktop/jobcontentoperators.csv"));//换成你的文件名 
-            String line = null;  
-            while((line=reader.readLine())!=null){ 
+            //deviceId  speId
+            BufferedReader reader = new BufferedReader(new FileReader("C:\\Users\\DELL\\Desktop\\重庆数据20190221/devices.csv"));//换成你的文件名 
+            String line = null;
+            Integer num = 0;
+            Integer numNull = 0;
+            while((line = reader.readLine()) != null){
                 line = line.replace("(", "");
                 line = line.replace(")", "");
                 line = line.replace(";", "");
                 String item[] = line.split(",");
-                if(map.keySet().contains(item[0])) {
-                    item[0] = map.get(item[0]);
-                }else {
-                   System.out.println(item[0]);
+                
+                String speName = "";
+                
+             
+                
+                if(item[1].trim().equals("NULL")) {
+                    speName = item[1];
+                    numNull += 1;
                 }
-                writer.add(item[0] + ","+ item[1]+ "\n");
+                else if(map.keySet().contains(item[1])) {
+                    speName = map.get(item[1]);
+
+                    Set<String> keySet = results.keySet();
+                    if(!keySet.contains(speName)) {
+                        results.put(speName, new LinkedList<>());
+                    }
+                    List<String> values = results.get(speName);
+                    values.add(item[0]);
+                    num += 1;
+                }else {
+
+                    System.out.println(item[0]);
+                }
+                
+            }
+            System.out.println("num : "+num);
+            System.out.println("numNull : "+numNull);
+            System.out.println("total : "+ (num + numNull ));
+            
+            
+            List<String> writer = new ArrayList<>();
+            
+            for(String keyIndex : results.keySet()) {
+                
+                String result = "UPDATE devices SET specification = \"" + keyIndex + "\"   where id in ( ";
+                
+                List<String> values = results.get(keyIndex);
+                
+                if(values.isEmpty()) {
+                    continue;
+                }
+                for(String value : values) {
+                    
+                    result += value + ",";
+                }
+                
+                result = result.substring(0, result.length() -1);
+                result += "); \n";
+                
+                writer.add(result);
             }
             
             
-            
+            //UPDATE devices SET specification = NULL where id = 801244;
+
             try {  
-                File csv = new File("C:/Users/DELL/Desktop/writers.csv"); // CSV数据文件 
+                File csv = new File("C:\\Users\\DELL\\Desktop\\重庆数据20190221/writers1.sql"); // CSV数据文件 
            
-                BufferedWriter bw = new BufferedWriter(new FileWriter(csv, true)); // 附加 
+                BufferedWriter bw = new BufferedWriter(new FileWriter(csv)); // 附加 
                 for(String key : writer ) {
                     
                     // 添加新的数据行 
